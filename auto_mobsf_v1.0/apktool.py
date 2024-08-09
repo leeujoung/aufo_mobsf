d2ef97982b26b07e927a142846f73f8e54b76f99
leeujoung <86176987+leeujoung@users.noreply.github.com>
2024-08-09 20:48:05 +0900

    Update apktool.py

--- auto_mobsf_v1.0/apktool.py
+++ auto_mobsf_v1.0/apktool.py
@@ -5,6 +5,7 @@
 import os
 import subprocess
 import decrypt_dex
+import shutil
 
 
 APKTOOL_PATH = 'apktool'
@@ -41,10 +42,14 @@ def decompile_apk(apk_path):
         subprocess.run(f"rmdir /s /q \"{output_dir}\"", shell=True)
 
     print("APK 디컴파일 중...")
-    run_command(f"{APKTOOL_PATH} d \"{apk_path}\" -o \"{output_dir}\" -f -s")
+    run_command(f"{APKTOOL_PATH} d \"{apk_path}\" -s -o \"{output_dir}\" -f")
     return output_dir
 
-def repackage_apk(decompiled_dir, apk_path):
+
+
+
+
+def repackage_apk(decompiled_dir, apk_path, export_dir):
     """디컴파일된 파일을 리패키징합니다."""
     # APK 파일 이름에 "_repack"을 추가하여 새로운 APK 경로 설정
     repackaged_apk_path = os.path.join(os.path.dirname(apk_path), os.path.basename(apk_path).replace('.apk', '_repack.apk'))
@@ -52,8 +57,67 @@ def repackage_apk(decompiled_dir, apk_path):
     print("APK 리패키징 중...")
     run_command(f"{APKTOOL_PATH} b \"{decompiled_dir}\" -o \"{repackaged_apk_path}\"")
     print(f"Repackaged APK created at: {repackaged_apk_path}")
+    
+    # # 리패키징된 APK를 export_dir에 복사
+    # export_apk_path = os.path.join(export_dir, os.path.basename(repackaged_apk_path))
+    # shutil.copy(repackaged_apk_path, export_apk_path)
+    # print(f"Copied repackaged APK to export directory: {export_apk_path}")
 
     return repackaged_apk_path
+
+
+
+#내부 APK 파일을 처리
+def process_internal_apk(assets_dir, key, export_dir):
+    
+    #추가
+    export_apk_path = None  # 초기화
+    
+    for filename in os.listdir(assets_dir):
+        if filename.endswith('.apk'):
+            internal_apk_path = os.path.join(assets_dir, filename)
+            print(f"Found internal APK: {internal_apk_path}")
+
+            # 내부 APK 디컴파일
+            internal_decompiled_dir = decompile_apk(internal_apk_path)
+            
+
+            # 복호화 수행
+            decrypt_dex.process_directory(internal_decompiled_dir, key)
+
+            # # 'kill' 이름이 포함된 원본 DEX 파일만 삭제
+            # for dex_file in os.listdir(internal_decompiled_dir):
+            #     dex_file_path = os.path.join(internal_decompiled_dir, dex_file)
+            #     if dex_file.endswith('.dex') and 'kill' in dex_file:
+            #         os.remove(dex_file_path)
+            #         print(f"Removed 'kill' named DEX file: {dex_file_path}")
+                    
+                    
+            # 리패키징 및 서명
+            internal_repackaged_apk = repackage_apk(internal_decompiled_dir, internal_apk_path, export_dir)
+            sign_apk(internal_repackaged_apk, 'my-release-key.jks', 'my-alias', '000000')
+
+            # 원본 위치에 덮어쓰기
+            shutil.move(internal_repackaged_apk, internal_apk_path)
+            print(f"Repackaged APK saved back to original location: {internal_apk_path}")
+
+            # 복사본 저장
+            export_apk_path = os.path.join(export_dir, filename)
+            shutil.copy(internal_apk_path, export_apk_path)
+            print(f"Copied repackaged APK to export directory: {export_apk_path}")
+            
+            # # '_repack'이 포함된 복사본만 저장
+            # if '_repack' in internal_apk_path:
+            #     export_apk_path = os.path.join(export_dir, filename)
+            #     shutil.copy(internal_apk_path, export_apk_path)
+            #     print(f"Copied repackaged APK to export directory: {export_apk_path}")
+
+            # 디컴파일된 폴더 삭제
+            shutil.rmtree(internal_decompiled_dir)
+            print(f"Deleted decompiled folder: {internal_decompiled_dir}")
+
+    ##추가
+    return export_apk_path  # 마지막으로 생성된 export_apk_path 반환
 
 def sign_apk(apk_path, keystore_path, alias, keystore_password):
     """APK 파일에 서명합니다."""
@@ -71,16 +135,26 @@ def main():
     # APK 디컴파일
     decompiled_dir = decompile_apk(apk_path)
     
+    # 내부 APK 처리
+    assets_dir = os.path.join(decompiled_dir, 'assets')
+    export_dir = os.path.join(os.path.dirname(apk_path), f"{os.path.splitext(os.path.basename(apk_path))[0]}_export")
+    os.makedirs(export_dir, exist_ok=True)
+
+    key = 'dbcdcfghijklmaop'  # AES 키
+    if os.path.exists(assets_dir):
+        process_internal_apk(assets_dir, key, export_dir)
+        
+    
     # 복호화 경로
     directory_path = decompiled_dir  
     # DEX 파일들이 있는 디렉토리 경로
     key = 'dbcdcfghijklmaop'  # AES 키
     decrypt_dex.process_directory(directory_path, key)
     
-    
+
     
     # 디컴파일된 파일을 리패키징
-    repackaged_apk_path = repackage_apk(decompiled_dir, apk_path)
+    repackaged_apk_path = repackage_apk(decompiled_dir, apk_path, export_dir)
 
     # APK 파일 서명
     keystore_path = 'my-release-key.jks'  # 서명 키 경로
